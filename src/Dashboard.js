a56955155915fbf945e5f2b4c9e76487b73bbb55
devchea <rchea817@gmail.com>
2020-07-19 13:24:57 -0700

    updates

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import Globe from './Globe'
+import NewsPanel from './NewsPanel'
+import StatPanel from './StatPanel'
+import { makeStyles, Paper, Grid} from "@material-ui/core";
+
+
+
+// export default function Dashboard() {
+  
+//   return (
+//     <div>
+//       <Globe/>
+    
+//       <Box m={20}>
+//         <NewsPanel/>
+//       </Box>
+    
+//       <StatPanel/>
+//     </div>
+//   );
+// }
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 3,
+  },
+  paper: {
+    padding: '1px',
+    margin: '1px',
+    textAlign: "center",
+  },
+}));
 
 export default function Dashboard() {
-  
-  return(
-    <div>
-      <Globe/>
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item>
+          {<Globe />}
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { render } from "react-dom";
-import Dashboard from "./Dashboard";
-import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import ButtonBase from '@material-ui/core/ButtonBase';
-
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
 
 export default function Home(props) {
-  
-  const history = props
 
   const handleClick = (props) => {
     props.history.push('/dashboard')
   }
 
-  const useStyles = makeStyles(()=> ({
-    buttonStyles: {
-      flex: 1
-    }
-  }))
-
-  const classes = useStyles()
-
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
         <Button
           variant="contained"
           color="primary"
-          onClick = {() => handleClick(history)}
+          onClick = {() => handleClick(props)}
           >
           Enter
         </Button>
-        {/* <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '70vh' }} /> */}
-      </Container>
-    </React.Fragment>
   );
 }
 
