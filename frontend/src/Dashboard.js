429490e78ddecc62630ff6fab1540a79850cac8d
devchea <rchea817@gmail.com>
2020-07-23 15:53:59 -0700

    working fetch with rendering issue

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import Globe from './Globe'
-import NewsPanel from './NewsPanel'
-import StatPanel from './StatPanel'
-import { makeStyles, Paper, Grid} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import Globe from "./Globe";
+import NewsPanel from "./NewsPanel";
+import { makeStyles, Paper, Grid } from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -10,26 +9,59 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 3,
   },
   paper: {
-    padding: '1px',
-    margin: '1px',
+    padding: "1px",
+    margin: "1px",
     textAlign: "center",
   },
 }));
 
+
+
 export default function Dashboard() {
+  const newsAPIKey = process.env.REACT_APP_NEWS_API_KEY
   const classes = useStyles();
+  const [news, setNews] = useState([]);
+  const [markers, setMarkers] = useState([]);
+
+  //test fetch
+  const fetchNews = async () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("dd", data);
+        setNews(data)
+        console.log('news:', news)
+      });
+  };
+
+  //real fetch
+  // const fetchNews = async() => {
+  //   await fetch(`https://gnews.io/api/v3/search?q=los$angeles%20covid%20&token=${newsAPIKey}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log("dd", data);
+  //       setNews(data);
+  //       console.log("news:", news);
+  //     });
+  // };
+
 
   return (
     <div className={classes.root}>
       <Grid container spacing={1}>
         <Grid item>
-          {<Globe />}
+          {<Globe fetchNews={fetchNews}/>}
           <Paper className={classes.paper}></Paper>
         </Grid>
       </Grid>
       <Grid container spacing={4}>
         <Grid item>
-          {<NewsPanel />}
+          {news.map (newsObj => (
+            <NewsPanel
+              key={newsObj.id}
+              name={newsObj.name}
+            />
+          ))}
           {/* <Paper className={classes.paper}></Paper> */}
         </Grid>
       </Grid>
--- frontend/src/NewsPanel.js
+++ frontend/src/NewsPanel.js
@@ -16,75 +16,77 @@
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 // import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+// import Typography from '@material-ui/core/Typography';
+// import ButtonBase from '@material-ui/core/ButtonBase';
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     margin: 'auto',
+//     maxWidth: 500,
+//   },
+//   image: {
+//     width: 128,
+//     height: 128,
+//   },
+//   img: {
+//     margin: 'auto',
+//     display: 'block',
+//     maxWidth: '100%',
+//     maxHeight: '100%',
+//   },
+// }));
 
-export default function ComplexGrid() {
-  const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src="../public/Images/Los_Angeles_1.jpeg" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Standard license
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  ID: 1030114
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Remove
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">$19.00</Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-  );
-}
+// export default function NewsPanel(props) {
+//   const classes = useStyles();
+//   console.log(props)
+
+//   return (
+//     <div className={classes.root}>
+//       <Paper className={classes.paper}>
+//         <Grid container spacing={2}>
+//           <Grid item>
+//             <ButtonBase className={classes.image}>
+//               <img className={classes.img} alt="complex" src="../public/Images/Los_Angeles_1.jpeg" />
+//             </ButtonBase>
+//           </Grid>
+//           <Grid item xs={12} sm container>
+//             <Grid item xs container direction="column" spacing={2}>
+//               <Grid item xs>
+//                 <Typography gutterBottom variant="subtitle1">
+//                   Standard license
+//                 </Typography>
+//                 <Typography variant="body2" gutterBottom>
+//                   Full resolution 1920x1080 • JPEG
+//                 </Typography>
+//                 <Typography variant="body2" color="textSecondary">
+//                   ID: 1030114
+//                 </Typography>
+//               </Grid>
+//               <Grid item>
+//                 <Typography variant="body2" style={{ cursor: 'pointer' }}>
+//                   Remove
+//                 </Typography>
+//               </Grid>
+//             </Grid>
+//             <Grid item>
+//               <Typography variant="subtitle1">$19.00</Typography>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </Paper>
+//     </div>
+//   );
+// }
 
 
 // const useStyles = makeStyles((theme) => ({
@@ -195,3 +197,55 @@ export default function ComplexGrid() {
 // }
 
 
+
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function ImgMediaCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
